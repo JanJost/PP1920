80b1c04fc249d07f9998df6a12464503885992d7
JanJost <38406598+JanJost@users.noreply.github.com>
2019-11-06 17:27:49 +0100

    added Heating strategy

---
+++ src/HeatingStrategy.kt
@@ -0,0 +1,35 @@
+interface HeatingStrategy{
+    fun needsHeating ( last10measurements : List<Float>) : Boolean
+}
+
+class InstantHeatingStrategy : HeatingStrategy {
+
+    override fun needsHeating(last10measurements: List<Float>): Boolean {
+        if(last10measurements.last()<19){
+            return true
+        }
+        return false
+    }
+}
+
+class SensibleHeatingStrategy : HeatingStrategy {
+    val underTwenty: (Float) -> Boolean ={it <20}
+    override fun needsHeating(last10measurements: List<Float>): Boolean {
+        if(last10measurements.any{underTwenty(it)}){
+            return true
+        }
+        return false
+    }
+}
+
+class ReasonableHeatingStrategy : HeatingStrategy {
+    private var counter =0
+    override fun needsHeating(last10measurements: List<Float>): Boolean {
+        last10measurements.forEach(){
+            if(it == (15).toFloat()){return true}
+            if(it == (19).toFloat()){counter++}
+            if(counter >=4){return true}
+            return false
+        }
+    }
+}
