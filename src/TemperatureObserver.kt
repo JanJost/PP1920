c7bb831b72c06b9d30ddf1508dc80c099609955f
JanJost <38406598+JanJost@users.noreply.github.com>
2019-11-07 21:45:47 +0100

    fertig
    Aufgabe 1 A und B

---
+++ src/TemperatureObserver.kt
@@ -0,0 +1,46 @@
+interface TemperatureObserver{
+    fun update(value :Float)
+}
+
+class TemperatureAlert : TemperatureObserver{
+    private val alertTemperature=40
+    override fun update(value: Float) {
+        if(value>alertTemperature){
+            println("Das ist aber heiß!!")
+        }
+    }
+}
+class WeatherReport : TemperatureObserver{
+    private var weatherData = mutableListOf<Float>()
+    private var counter=0
+    override fun update(value: Float) {
+        weatherData.add(value)
+        counter++
+        if(counter==100){
+            println("die letzten hundert Temperaturen: ")
+            for(i in weatherData){
+                println(i)
+            }
+            weatherData.clear()
+            counter=0
+        }
+    }
+}
+class HeatingSystem : TemperatureObserver{
+    private var weatherData = mutableListOf<Float>()
+    private var counter=0
+    private var HeatingStrategy: HeatingStrategy =ReasonableHeatingStrategy()
+    override fun update(value: Float) {
+        weatherData.add(value)
+        counter++
+        if(counter==10){
+            if(HeatingStrategy.needsHeating(weatherData)){
+                println("Heizung an")
+            }
+            else{
+                println("Heizung aus")
+            }
+
+        }
+    }
+}
---
+++ src/app.kt
@@ -0,0 +1,18 @@
+fun main(){
+    val tm1 = Thermometer(RoundValues(RandomSensor()))
+    val tm2 = Thermometer(SensorLimits(UpDownSensor()))
+    val tm3 = Thermometer(IgnoreDublicates(UpDownSensor()))
+
+    tm1.register(WeatherReport())
+    tm1.register(HeatingSystem())
+    tm2.register(TemperatureAlert())
+    tm2.register(WeatherReport())
+    tm3.register(WeatherReport())
+    tm3.register(HeatingSystem())
+
+    tm1.measure(10)
+    tm2.measure(1000)
+    tm3.measure(103)
+
+
+}
