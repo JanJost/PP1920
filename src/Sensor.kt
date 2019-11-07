328ab0a03cdd8a912daebbccaf1a01109a8e0b18
JanJost <33106678+JaypsD@users.noreply.github.com>
2019-11-07 15:41:48 +0100

    Sensor

---
+++ src/Sensor.kt
@@ -0,0 +1,21 @@
+interface Sensor{
+    fun getTemperatur() : Float
+}
+
+class RandomSensor : Sensor{
+    override fun getTemperatur(): Float {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+class UpDownSensor : Sensor{
+    override fun getTemperatur(): Float {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+open class SensorLimit (val decoratedSensor: Sensor){
+    override fun getTemperatur(): Float{
+        
+    }
+}
